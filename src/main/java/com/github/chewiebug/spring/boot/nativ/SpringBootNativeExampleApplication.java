22c3db33d30a6d73e5c6dbe0287dfee436e14d27
chewiebug <jwu@gmx.ch>
2022-04-23 18:35:39 +0200

    some tuning
    - minimal jre base image
    - improve load test

--- src/main/java/com/github/chewiebug/spring/boot/nativ/SpringBootNativeExampleApplication.java
+++ src/main/java/com/github/chewiebug/spring/boot/nativ/SpringBootNativeExampleApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootNativeExampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("maxMem=" + Runtime.getRuntime().maxMemory() + ", " + Runtime.getRuntime().maxMemory() / 1024 / 1024 + "MB");
+		System.out.println("availableProcessors=" + Runtime.getRuntime().availableProcessors());
+
 		SpringApplication.run(SpringBootNativeExampleApplication.class, args);
 	}
 
